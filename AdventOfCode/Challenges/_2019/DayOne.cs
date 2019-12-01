d2b3f7c2293417869eaa4ffa1d2986492fe30dfb
AKTheKnight <alex@aktheknight.co.uk>
2019-12-01 09:28:13 +0000

    2019 Day 1

--- AdventOfCode/Challenges/_2019/DayOne.cs
+++ AdventOfCode/Challenges/_2019/DayOne.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Challenges._2019
 {
@@ -13,12 +15,41 @@ namespace AdventOfCode.Challenges._2019
 
         public override string SolutionOne()
         {
-            throw new System.NotImplementedException();
+            var total = Input
+                .Select(s => int.Parse(s))
+                .Select(val => ((int) Math.Floor(val / 3.0)) - 2)
+                .Sum();
+
+            return total.ToString();
         }
 
         public override string SolutionTwo()
         {
-            throw new System.NotImplementedException();
+            var total = 0;
+            
+            foreach (var s in Input)
+            {
+                var valtotal = 0;
+                var val = int.Parse(s);
+
+                while (val != 0)
+                {
+                    val = GetFuel(val);
+                    valtotal += val;
+                }
+
+                total += valtotal;
+            }
+
+            return total.ToString();
         }
+
+        private int GetFuel(int s)
+        {
+            //Divide by 3 and round down then sub 2
+            //Max val from 0 or val. COnverts negatives to 0
+            return Math.Max(0, ((int) Math.Floor(s / 3.0)) - 2);
+        }
+        
     }
 }
