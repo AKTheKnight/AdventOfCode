c2dea471e0a3a16679488fb56d150d4b86b0c73e
Alex Knight <alex@aktheknight.co.uk>
2020-12-05 12:18:45 +0000

    2020 Day 2-5 files

---
+++ AdventOfCode/Challenges/_2020/DayFour.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Challenges._2020
+{
+    public class DayFour2020 : Challenge
+    {
+        private static List<string> Input { get; set; }
+        public DayFour2020() : base(4)
+        {
+            if (Input == null)
+                Input = Utils.GetInput(2020, 4);
+        }
+
+        public override string SolutionOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SolutionTwo()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ AdventOfCode/Challenges/_2020/DayThree.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Challenges._2020
+{
+    public class DayThree2020 : Challenge
+    {
+        private static List<string> Input { get; set; }
+        public DayThree2020() : base(3)
+        {
+            if (Input == null)
+                Input = Utils.GetInput(2020, 3);
+        }
+
+        public override string SolutionOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SolutionTwo()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
